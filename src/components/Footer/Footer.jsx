a75061ce2754e243bfa5f873d52afb2a758dddc9
박광주 <kjpar_kr@yahoo.co.kr>
2019-01-10 18:33:26 +0900

    처음 구성

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -39,9 +39,6 @@ function Footer({ ...props }) {
                 <p className={classes.right}>
           <span>
             &copy; {1900 + new Date().getYear()}{" "}
-              <a href="https://www.creative-tim.com" className={classes.a}>
-              Creative Tim
-            </a>, made with love for a better web
           </span>
                 </p>
             </div>
