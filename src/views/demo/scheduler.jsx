1733c7120831c8c80fa7b373c130b392b3b41510
박광주 <kjpar_kr@yahoo.co.kr>
2019-01-15 23:30:19 +0900

    스케쥴 수정

--- src/views/demo/scheduler.jsx
+++ src/views/demo/scheduler.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import Calendar from "react-big-calendar";
 import moment from 'moment';
+import 'moment/locale/ko';
 import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
 
 import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
@@ -10,6 +11,8 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 // @material-ui/core
 import withStyles from "@material-ui/core/styles/withStyles";
 import mainStyle from "assets/jss/material-dashboard-react/views/dashboardStyle.jsx";
+
+//moment.lang('ko'); // 언어팩 변경
 
 const localizer = Calendar.momentLocalizer(moment);
 const DnDCalendar = withDragAndDrop(Calendar);
@@ -20,13 +23,36 @@ class BigSchedulerPage extends React.Component {
           {
             start: new Date(),
             end: new Date(moment().add(1, "days")),
-            title: "Some title"
+            title: "Some title1",
+            resource: "A"
+          },
+          {
+            start: new Date(),
+            end: new Date(moment().add(1, "days")),
+            title: "Some title2",
+            resource: "B"
           }
-        ]
+        ],
+    };
+
+    onSelectEvent = ({ title, start, end, allDay, resource  }) => {
+        console.log("onSelectEvent" + resource);
+
+    
+        // change the border color just for fun
+        //$(this).css('border-color', 'red');
+    };
+
+    onDoubleClickEvent = ({title, start, end, allDay}) => {
+        console.log("onDoubleClickEvent" + title);
+    };
+
+    onSelectSlot = ({ title, start, end, allDay, resource }) => {
+        console.log("onSelectSlot" + resource);
     };
 
     // 일정 리사이즈
-    onEventResize = ({ event, start, end, allDay }) => {
+    onEventResize = ({ title, start, end, allDay }) => {
         this.setState(state => {
             state.events[0].start = start;
             state.events[0].end = end;
@@ -54,7 +80,11 @@ class BigSchedulerPage extends React.Component {
                     events={this.state.events}
                     onEventDrop={this.onEventDrop}
                     onEventResize={this.onEventResize}
+                    onSelectEvent={this.onSelectEvent}
+                    onDoubleClickEvent={this.onDoubleClickEvent}
+                    onSelectSlot={this.onSelectSlot}
                     resizable
+                    selectable
                     style={{ height: "100vh" }}
                 />
             </div>
